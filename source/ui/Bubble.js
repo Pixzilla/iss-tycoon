443074ae34df161b07e7f38839a4c13860f28d16
Christoph Martens <christoph@martens.ms>
2015-04-12 00:02:17 +0200

    ASDASD

--- source/ui/Bubble.js
+++ source/ui/Bubble.js
@@ -4,7 +4,8 @@ lychee.define('game.ui.Bubble').includes([
 ]).exports(function(lychee, global, game, attachments) {
 
 	var _font    = attachments["fnt"];
-//	var _texture = attachments["png"];
+	var _config  = attachments["json"].buffer;
+	var _texture = attachments["png"];
 
 
 	var Class = function(data) {
@@ -12,11 +13,11 @@ lychee.define('game.ui.Bubble').includes([
 		var settings = lychee.extend({}, data);
 
 
-		this.key        = 'Foo';
-		this.value      = 'Bar';
+		this.key        = settings.key   || 'urine';
+		this.value      = settings.value || '0%';
 
 
-		settings.radius = 64;
+		settings.radius = 48;
 		settings.shape  = lychee.ui.Entity.SHAPE.circle;
 
 
@@ -35,12 +36,12 @@ lychee.define('game.ui.Bubble').includes([
 			var radius   = this.radius;
 
 
-			renderer.setAlpha(0.2);
+			renderer.setAlpha(0.6);
 
 			renderer.drawCircle(
 				position.x + offsetX,
 				position.y + offsetY,
-				radius - 2,
+				radius - 1,
 				'#00000',
 				true
 			);
@@ -52,21 +53,32 @@ lychee.define('game.ui.Bubble').includes([
 				position.y + offsetY,
 				radius,
 				'#0ba2ff',
-				false
+				false,
+				1
 			);
+
+
+			var map = _config.map[this.key] || null;
+			if (map !== null) {
+
+				var frame = map[0] || null;
+				if (frame !== null) {
+
+					renderer.drawSprite(
+						position.x + offsetX - 16,
+						position.y + offsetY - 30,
+						_texture,
+						frame
+					);
+
+				}
+
+			}
 
 
 			renderer.drawText(
 				position.x + offsetX,
-				position.y + offsetY - 32,
-				this.key,
-				_font,
-				true
-			);
-
-			renderer.drawText(
-				position.x + offsetX,
-				position.y + offsetY + 16,
+				position.y + offsetY + 12,
 				this.value,
 				_font,
 				true
