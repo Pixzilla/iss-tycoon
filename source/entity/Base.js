0eea6f1b65ec75cbf96493dd8d4eb94edb2c46bc
Christoph Martens <christoph@martens.ms>
2015-04-11 12:59:54 +0200

    Added Base Entity

---
+++ source/entity/Base.js
@@ -0,0 +1,41 @@
+
+lychee.define('game.entity.Base').includes([
+	'lychee.game.Sprite'
+]).exports(lychee, global, game, attachments) {
+
+	var _config  = attachments["json"];
+	var _texture = attachments["png"];
+
+
+	var Class = function(data) {
+
+		var settings = lychee.extend({}, data);
+
+
+		this.air         = 0;
+		this.power       = 0;
+		this.temp        = 0;
+
+		this.connections = [ null, null, null, null ];
+
+
+
+		settings.texture = settings.texture || _texture;
+		settings.width   = settings.width   || _config.width;
+		settings.height  = settings.height  || _config.height;
+
+
+		lychee.game.Sprite.call(this, settings);
+
+	};
+
+
+	Class.prototype = {
+
+	};
+
+
+	return Class;
+
+});
+
---
+++ source/entity/Room.js
@@ -0,0 +1,7 @@
+
+lychee.define('game.entity.Base').includes([
+	'lychee.game.Sprite'
+]).exports(lychee, global, game, attachments) {
+
+});
+
